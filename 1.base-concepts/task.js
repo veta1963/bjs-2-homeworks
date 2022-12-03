3926b30353883342472113604a0dfe6d3d4aae3a
veta1963 <seagull-v@bk.ru>
2022-12-03 10:41:11 +0500

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2-4*a*c;
+  if (d>0) {
+    let d1=(-b + Math.sqrt(d))/(2*a);
+    let d2=(-b - Math.sqrt(d))/(2*a);
+    alert(d1);
+    alert(d2); 
   
-  return arr;
+  } else if (d==0) {
+    let d3=(-b/(2*a));
+    alert(d3);
+    
+  } else
+  return; {
+    return arr;
+  }
+  
+  
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = percent/12;
+  let S = contribution;
+  let amount;
+  let n = countMonths;
+  isNaN("percent");
+  isNaN("contribution");
+  isNaN("amount");
+  isNaN("countMonths");
+  function amount(){
+    let amountMonth = S * (P + (P / (((1 + P)**n) - 1)));
+    amount = amountMonth*n;
+  } 
+  amount();
 }
