03532ef24cc036893e5d3035bb1a082fceb4f66d
veta1963 <seagull-v@bk.ru>
2022-12-05 19:50:34 +0500

    Change commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d1;
-  let d2;
-  let d = Math.pow(b,2) - 4 * a * c;
+  const arr = [];
+  let discriminantFirst;
+  let discriminantSecond;
+  const d = Math.pow(b,2) - 4 * a * c;
 
   if (d>0) {
-    d1 = (-b + Math.sqrt(d))/(2*a);
-    d2 = (-b - Math.sqrt(d))/(2*a);
-    arr.push(d1);
-    arr.push(d2); 
+    discriminantFirst = (-b + Math.sqrt(d))/(2*a);
+    discriminantSecond = (-b - Math.sqrt(d))/(2*a);
+    arr.push(discriminantFirst);
+    arr.push(discriminantSecond); 
     console.log("Первый корень: " + arr[0] + "Второй корень: " + arr[1]);
 
   } else if (d===0) {
-    d1 = (-b/(2*a));
-    arr.push(d1);
+    discriminantFirst = (-b/(2*a));
+    arr.push(discriminantFirst);
     console.log(arr);
 
   } else
@@ -24,17 +24,24 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent/12;
-  let S = contribution;
-  let amount;
-  let n = countMonths;
-  isNaN("percent");
-  isNaN("contribution");
-  isNaN("amount");
-  isNaN("countMonths");
-  function amount(){
-    let amountMonth = S * (P + (P / (((1 + P)**n) - 1)));
-    amount = amountMonth*n;
-  } 
-  amount();
+  let bodyCredit;
+  let monthPayment;
+  let totalAmount;
+
+ if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths)
+    ) {
+       console.log('Один из параметров содержит неверное значение');
+       return false;
+    }
+
+  percent = (percent / 100) / 12;
+  bodyCredit = amount - contribution;
+  monthPayment = bodyCredit * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  totalAmount = parseFloat((monthPayment * countMonths).toFixed(2));
+
+  return totalAmount;
 }
