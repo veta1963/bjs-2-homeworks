cbf0db43b8df27789e57229d1d9a1a3e0cd08f8e
veta1963 <seagull-v@bk.ru>
2022-12-18 19:42:13 +0500

    Четвёртое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age){      
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];   
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function (subjectName) {    
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks.length <= 0) {
+    return 0;
+  } 
+  else if (this.marks.length > 0) {
+    this.marks.push(...marks);
+  } 
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length <= 0) {
+      return 0;
+    } 
+    else if (this.marks.length > 0) {  
+      let sum = 0;
+      for (let i=0; i< this.marks.length; i++) {
+      sum += this.marks[i];
+    }  
+    return sum / this.marks.length;
+    }
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function (reason) { 
+    this.exsluded = reason;
+    delete this.subject;
+    delete this.marks; 
 }
+
