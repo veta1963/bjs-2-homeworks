dbce5ede243bf3d6fede8e097b746705232f222a
veta1963 <seagull-v@bk.ru>
2023-01-04 10:55:09 +0500

    Четвёртое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,22 +10,22 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.marks) {
-    return 0;
-  } 
+  if (!this.marks) {
+    return;
+} 
     this.marks.push(...marks);
    
 }
 
 Student.prototype.getAverage = function () {
-    if (this.marks || this.marks.length === 0) {
-      return 0;
+    if (!this.marks) {
+      return;
     } 
       let sum = 0;
       for (let i = 0; i < this.marks.length; i++) {
       sum += this.marks[i];
     }  
-    return sum/this.marks.length;
+    return (sum / this.marks.length);
     
 }
 
@@ -34,3 +34,16 @@ Student.prototype.exclude = function (reason) {
     delete this.marks; 
     this.excluded = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
