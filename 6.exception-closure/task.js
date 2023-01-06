06d361dcfd587aeeca8f5e54b7c30c2023796507
veta1963 <seagull-v@bk.ru>
2023-01-06 16:53:22 +0500

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(a) {    
+    Number.parseFloat(a);
+    if (a === NaN) {
+        console.log("Невалидное значение");
+    }    
+    try{        
+        throw new Error("ошибка!")
+    } catch (error){       
+        console.log(error);
+    }
+    return a;
+}
+parseCount();
+
+function validateCount(number) {
+    parseCount(number);
+    if(number !==NaN) {
+        return number;
+    }
+    
+}
+
+class Triangle {
+    constructor(a,b,c) {
+        if(a+b < c || b+c < a || a+c < b) {
+            throw new Error ("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        let p = a + b + c;
+        return p;
+    }
+    get area() {
+        let x = p/2(p/2 - a)*(p/2 - b)*(p/2 - c)
+        let S = Math.sqrt(x);
+        return S.toFixed(3);        
+    }
+}
+
+function getTriangle(a, b, c) {  
+    Triangle = new Triangle(a, b, c); 
+    try {       
+    } catch (perimeter) {
+            return ("Ошибка! Треугольник не существует");
+        }    
+    try {
+    } catch (area) {
+            return ("Ошибка! Треугольник не существует")
+        }
+}
